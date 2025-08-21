f8c013ecbd36303e914aa811306bff7b92d5bf36
foag <cursotecnicorrkm@gmail.com>
2025-08-21 10:50:53 -0400

    iiii
    oooooooooooooooo

--- plano/plano.php
+++ plano/plano.php
@@ -17,16 +17,18 @@ if(file_exists($arquivo)) {
     <title>Planos - Vortex Academy</title>
     <link rel="stylesheet" href="planos.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <header>
         <div class="logo">Vortex</div>
         <nav>
             <ul>
-                <li><a href="index.php">Início</a></li>
-                <li><a href="planos.php">Planos</a></li>
-                <li><a href="#">Treinos</a></li>
-                <li><a href="#">Contato</a></li>
+  <li><a href="//index.php">Início</a></li>
+  <li><a href="planos.php">Planos</a></li>
+  <li><a href="treinos.php">Treinos</a></li>
+  <li><a href="contato.php">Contato</a></li>
+
             </ul>
         </nav>
     </header>
@@ -40,7 +42,11 @@ if(file_exists($arquivo)) {
         <?php foreach($planos as $plano): ?>
             <div class="plano-card">
                 <h2><?= htmlspecialchars($plano['nome']) ?></h2>
+                <?php if (is_numeric($plano['preco'])): ?>
                 <div class="preco">R$ <?= number_format($plano['preco'], 2, ',', '.') ?></div>
+             <?php else: ?>
+                <div class="preco"><?= htmlspecialchars($plano['preco']) ?></div>
+              <?php endif; ?>
                 <ul>
                     <?php foreach($plano['descricao'] as $item): ?>
                         <li><?= htmlspecialchars($item) ?></li>
---
+++ plano/treinos.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Treinos</title>
+</head>
+<body>
+  <h1>Página de Treinos</h1>
+  <p>Aqui você pode colocar os treinos.</p>
+  <a href="index.php">Voltar ao Início</a>
+</body>
+</html>
